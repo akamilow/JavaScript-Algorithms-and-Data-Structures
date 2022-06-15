cc3478207d4c55107fae4927cd9bfacb1e4185c2
Camilo Castellar <camilocastelar3@gmail.com>
2022-06-15 17:28:58 -0500

    Match Anything with Wildcard Period

--- Regular-Expressions/regex.js
+++ Regular-Expressions/regex.js
@@ -44,3 +44,18 @@ And here match returns the value ["Repeat", "Repeat", "Repeat"]
 let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle/ig; // Change this line
 let Result = twinkleStar.match(starRegex);; // Change this line
+
+// Match Anything with Wildcard Period
+/**
+ * let humStr = "I'll hum a song";
+let hugStr = "Bear hug";
+let huRegex = /hu./;
+huRegex.test(humStr);
+huRegex.test(hugStr);
+Both of these test calls would return true.
+ * 
+ */
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; // Change this line
+let reSult = unRegex.test(exampleStr);
