b6d499df85c8f16f2820f9c51e396fcd3180e743
Camilo Castellar <camilocastelar3@gmail.com>
2022-07-07 16:01:16 -0500

    Remove Items Using splice()

--- Basic-Data-Structures/basic-data-structures.js
+++ Basic-Data-Structures/basic-data-structures.js
@@ -28,3 +28,14 @@ function mixedNumbers(arr) {
   }
   console.log(popShift(['challenge', 'is', 'not', 'complete']));
 // [ 'challenge', 'complete' ]
+
+// Remove Items Using splice()
+/**
+ * first parameter represents the index on the array from which to begin removing elements,
+ *  while the second parameter indicates the number of elements to delete
+ */
+ const arr = [2, 4, 5, 1, 7, 5, 2, 1];
+ // Only change code below this line
+ arr.splice(1,4);
+ // Only change code above this line
+ console.log(arr);
