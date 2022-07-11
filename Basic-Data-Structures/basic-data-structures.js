9bb969d77da4136cf8a73d1a2bd9ae16b415486d
Camilo Castellar <camilocastelar3@gmail.com>
2022-07-11 11:38:51 -0500

    Combine Arrays with the Spread Operator

--- Basic-Data-Structures/basic-data-structures.js
+++ Basic-Data-Structures/basic-data-structures.js
@@ -70,3 +70,11 @@ function copyMachine(arr, num) {
   return newArr;
 }
 console.log(copyMachine([true, false, true], 2));
+
+// Combine Arrays with the Spread Operator
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence =  ['learning', ...fragment, 'is', 'fun']; // Change this line
+  return sentence;
+}
+console.log(spreadOut()); // [ 'learning', 'to', 'code', 'is', 'fun' ]
