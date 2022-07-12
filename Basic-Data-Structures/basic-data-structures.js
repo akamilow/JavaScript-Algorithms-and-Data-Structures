4c1568ec08038c1fd178b44a12a759f41a2b251c
Camilo Castellar <camilocastelar3@gmail.com>
2022-07-12 13:06:37 -0500

    Generate an Array of All Object Keys with Object.keys()

--- Basic-Data-Structures/basic-data-structures.js
+++ Basic-Data-Structures/basic-data-structures.js
@@ -222,3 +222,30 @@ function countOnline(usersObj) {
   // Only change code above this line
 }
 console.log(countOnline(userss));
+
+// Generate an Array of All Object Keys with Object.keys()
+let usersss = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+  return Object.keys(obj);
+  // Only change code above this line
+}
+console.log(getArrayOfUsers(usersss));
