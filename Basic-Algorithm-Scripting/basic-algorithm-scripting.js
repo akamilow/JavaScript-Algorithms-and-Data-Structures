c5f4d016ffedf235ef8e240f9c14cf5c0ac881ab
Camilo Castellar <camilocastelar3@gmail.com>
2022-07-15 13:24:23 -0500

    Return Largest Numbers in Arrays

--- Basic-Algorithm-Scripting/basic-algorithm-scripting.js
+++ Basic-Algorithm-Scripting/basic-algorithm-scripting.js
@@ -37,3 +37,20 @@ function findLongestWordLength(str) {
     return longest;
 }
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+// Return Largest Numbers in Arrays
+function largestOfFour(arr) {
+    let largest = [];
+    for (let i = 0; i < arr.length; i++) {
+        let largestNum = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > largestNum) {
+                largestNum = arr[i][j];
+            }
+        }
+        largest.push(largestNum);
+    }
+    return largest;
+}
+  
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
