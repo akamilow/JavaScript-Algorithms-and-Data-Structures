97d32073e41381d82feaf7e0c0bca9be0e2f147a
Camilo Castellar <camilocastelar3@gmail.com>
2022-07-15 13:11:07 -0500

    Find the Longest Word in a String

--- Basic-Algorithm-Scripting/basic-algorithm-scripting.js
+++ Basic-Algorithm-Scripting/basic-algorithm-scripting.js
@@ -24,3 +24,16 @@ function factorialize(num) {
     return num;
 }
 factorialize(5);
+
+// Find the Longest Word in a String
+function findLongestWordLength(str) {
+    let words = str.split(' ');
+    let longest = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longest) {
+            longest = words[i].length;
+        }
+    }
+    return longest;
+}
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
