2db0a9babf2c42a5ee5930b3fd76d08a83e4878d
Camilo Castellar <camilocastelar3@gmail.com>
2022-07-15 12:58:19 -0500

    Factorialize a Number

--- Basic-Algorithm-Scripting/basic-algorithm-scripting.js
+++ Basic-Algorithm-Scripting/basic-algorithm-scripting.js
@@ -10,3 +10,17 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 reverseString("hello");
+
+// Factorialize a Number
+// For example: 5 = 1 * 2 * 3 * 4 * 5 = 120
+function factorialize(num) {
+    if (num === 0) {
+        return 1;
+    } else {
+        for (let i = num - 1; i >= 1; i--) {   
+            num *= i;
+        }
+    }
+    return num;
+}
+factorialize(5);
