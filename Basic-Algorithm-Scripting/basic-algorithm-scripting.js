83d872e73f55e91b7460231da5daf15a77d4f9f3
Camilo Castellar <camilocastelar3@gmail.com>
2022-07-25 15:15:04 -0500

    Chunky Monkey

--- Basic-Algorithm-Scripting/basic-algorithm-scripting.js
+++ Basic-Algorithm-Scripting/basic-algorithm-scripting.js
@@ -184,3 +184,13 @@ function mutation(arr) {
     return true;
 }
 console.log(mutation(["hello", "Hello"]));
+
+// Chunky Monkey
+function chunkArrayInGroups(arr, size) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+        newArr.push(arr.slice(i, i + size));
+    }
+    return newArr;
+}
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
