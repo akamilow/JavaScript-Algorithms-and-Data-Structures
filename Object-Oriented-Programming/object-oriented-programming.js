274e658d5928f182face2776808325cb7bd55fa6
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-02 15:28:11 -0500

    Use a Constructor to Create Objects

--- Object-Oriented-Programming/object-oriented-programming.js
+++ Object-Oriented-Programming/object-oriented-programming.js
@@ -37,3 +37,12 @@ function Dog() {
     this.color = "blue";
     this.numLegs = 2;
 }
+
+// Use a Constructor to Create Objects
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+// Only change code below this line
+let hound = new Dog();
