39e84aee49ecd410f93d56a68093a426c9efdc8e
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-04 15:31:53 -0500

    Understand the Constructor Property

--- Object-Oriented-Programming/object-oriented-programming.js
+++ Object-Oriented-Programming/object-oriented-programming.js
@@ -105,3 +105,16 @@ for (let property in beagle) {
 }
 //console.log(ownProps);
 //console.log(prototypeProps);
+
+// Understand the Constructor Property
+function Dog(name) {
+    this.name = name;
+}
+// Only change code below this line
+function joinDogFraternity(candidate) {
+    if (candidate.constructor === Dog) {
+      return true;
+    } else {
+      return false;
+    }
+}
