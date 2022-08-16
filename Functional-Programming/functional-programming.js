c170d865a2ce6a140007ea043236c529da2251d9
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-16 17:05:12 -0500

    Refactor Global Variables Out of Function

--- Functional-Programming/functional-programming.js
+++ Functional-Programming/functional-programming.js
@@ -99,3 +99,28 @@ function incrementer(arg) {
     return arg + 1;
     // Only change code above this line
 }
+
+// Refactor Global Variables Out of Functions
+// The global variable
+const bookList = [
+    "The Hound of the Baskervilles", 
+    "On The Electrodynamics of Moving Bodies", 
+    "Philosophiæ Naturalis Principia Mathematica", 
+    "Disquisitiones Arithmeticae"
+];
+// Change code below this line
+function add(arr, bookName) {
+  let newArr = [...arr]
+  newArr.push(bookName);
+  return newArr;
+  // Change code above this line
+}
+// Change code below this line
+function remove(arr, bookName) {
+    let newArr = [...arr]
+    if (newArr.indexOf(bookName) >= 0) {
+        newArr.splice(newArr.indexOf(bookName), 1);
+        return newArr;
+    }
+}
+// Change code above this line
