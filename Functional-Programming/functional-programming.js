6322319b470647165f2eba29adae733648dfdc12
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-18 15:05:31 -0500

    Remove Elements from an Array Using slice Instead of splice

--- Functional-Programming/functional-programming.js
+++ Functional-Programming/functional-programming.js
@@ -412,3 +412,10 @@ const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
 sliceArray(inputAnim, 1, 3);
 
 // Remove Elements from an Array Using slice Instead of splice
+function nonMutatingSplice(cities) {
+  // Only change code below this line
+  return cities.slice(0, 3);
+  // Only change code above this line
+}
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
