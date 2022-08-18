93b616a8d1e9c72e70097ac438ca35ae86553b36
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-18 15:01:22 -0500

    Return Part of an Array Using the slice Method

--- Functional-Programming/functional-programming.js
+++ Functional-Programming/functional-programming.js
@@ -401,3 +401,12 @@ Array.prototype.myFilter = function(callback) {
 const new_ss = s.myFilter(function(item) {
   return item % 2 === 1;
 });
+
+// Return Part of an Array Using the slice Method
+function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+  return anim.slice(beginSlice, endSlice);
+  // Only change code above this line
+}
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
