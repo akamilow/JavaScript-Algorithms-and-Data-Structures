9238a9ad4840a4b717ddc936b05739fba5406783
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-22 16:11:38 -0500

    Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem

--- Functional-Programming/functional-programming.js
+++ Functional-Programming/functional-programming.js
@@ -565,3 +565,14 @@ function getRating(watchListt) {
   return averageRating;
 }
 console.log(getRating(watchListt));
+
+// Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
+const squareList = arr => {
+  // Only change code below this line
+  return arr
+          .filter(num => num > 0 && num % parseInt(num) === 0)
+          .map(num => Math.pow(num, 2));
+  // Only change code above this line
+};
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
