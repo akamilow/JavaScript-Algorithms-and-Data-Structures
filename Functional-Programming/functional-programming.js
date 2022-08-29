68955353b2d77f91d6074d2f4ca7818388369d78
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-29 12:18:35 -0500

    Split a String into an Array Using the split Method

--- Functional-Programming/functional-programming.js
+++ Functional-Programming/functional-programming.js
@@ -595,3 +595,11 @@ function nonMutatingSort(arr) {
   // Only change code above this line
 }
 nonMutatingSort(globalArray);
+
+// Split a String into an Array Using the split Method
+function splitify(str) {
+  // Only change code below this line
+  return str.split(/\W/)
+  // Only change code above this line
+}
+splitify("Hello World,I-am code");
