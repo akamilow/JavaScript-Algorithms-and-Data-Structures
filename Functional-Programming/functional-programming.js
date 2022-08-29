4e31361953cb4bd9230eb7d4693f4838e43940b4
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-29 12:34:03 -0500

    Apply Functional Programming to Convert Strings to URL Slugs

--- Functional-Programming/functional-programming.js
+++ Functional-Programming/functional-programming.js
@@ -611,3 +611,15 @@ function sentensify(str) {
   // Only change code above this line
 }
 sentensify("May-the-force-be-with-you");
+
+// Apply Functional Programming to Convert Strings to URL Slugs
+// Only change code below this line
+function urlSlug(title) {
+  return title
+    .toLowerCase()
+    .trim()
+    .split(/\s+/)
+    .join("-");
+}
+// Only change code above this line
+urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
