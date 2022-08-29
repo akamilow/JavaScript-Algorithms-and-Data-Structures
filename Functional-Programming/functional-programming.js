64d77a4558af4b26e38fb395ba0d0c448433523c
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-29 12:23:23 -0500

    Combine an Array into a String Using the join Method

--- Functional-Programming/functional-programming.js
+++ Functional-Programming/functional-programming.js
@@ -603,3 +603,11 @@ function splitify(str) {
   // Only change code above this line
 }
 splitify("Hello World,I-am code");
+
+// Combine an Array into a String Using the join Method
+function sentensify(str) {
+  // Only change code below this line
+  return str.split(/\W/).join(" ");
+  // Only change code above this line
+}
+sentensify("May-the-force-be-with-you");
