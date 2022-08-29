5542bbc8f323d40ac98ea867c99a6f48ebd15064
Camilo Castellar <camilocastelar3@gmail.com>
2022-08-29 12:14:09 -0500

    Return a Sorted Array Without Changing the Original Array

--- Functional-Programming/functional-programming.js
+++ Functional-Programming/functional-programming.js
@@ -584,3 +584,14 @@ function alphabeticalOrder(arr) {
   // Only change code above this line
 }
 alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+
+// Return a Sorted Array Without Changing the Original Array
+const globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  return [].concat(arr).sort(function(a, b) {
+    return a - b;
+  });
+  // Only change code above this line
+}
+nonMutatingSort(globalArray);
